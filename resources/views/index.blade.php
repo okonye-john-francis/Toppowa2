caa2033c556eb3b21a3ec30f12a0e29c13d77e71
Okonye <francis.jero2@gmail.com>
2018-09-01 14:59:32 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,415 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Medilab Free Bootstrap HTML5 Template</title>
+  <meta name="description" content="Free Bootstrap Theme by BootstrapMade.com">
+  <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
+
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
+  <link rel="stylesheet" type="text/css" href="{{URL::asset('Assets/css/font-awesome.min.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{URL::asset('Assets/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" type="text/css" href="{{URL::asset('Assets/css/style.css')}}">
+  <!-- =======================================================
+    Theme Name: Medilab
+    Theme URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
+    Author: BootstrapMade.com
+    Author URL: https://bootstrapmade.com
+  ======================================================= -->
+
+  <style type="text/css">
+  	
+  		.jstfy {
+  		    text-align: justify;
+  		    text-justify: inter-word;
+  		}
+
+  </style>
+</head>
+
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="60">
+  <!--banner-->
+  <section id="banner" class="banner">
+    <div class="bg-color">
+      <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+          <div class="col-md-12">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				        <span class="icon-bar"></span>
+				      </button>
+              <a class="navbar-brand" href="#"><img src="img/logo.png" class="img-responsive" style="width: 140px; margin-top: -16px;"></a>
+            </div>
+            <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#banner">Home</a></li>
+                <li class=""><a href="#service">Services</a></li>
+                <li class=""><a href="#about">About</a></li>
+                <li class=""><a href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div class="container">
+        <div class="row">
+          <div class="banner-info">
+            <div class="banner-logo text-center">
+              <!-- <img src="img/d.jpg" class="img-responsive"> -->
+            </div>
+            <div class="banner-text text-center">
+              <h1 class="white">Healthcare at your desk!!</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod <br>tempor incididunt ut labore et dolore magna aliqua.</p>
+              <a href="#contact" class="btn btn-appoint">Make an Appointment.</a>
+            </div>
+            
+              <a href="#service"><i class="fa fa-angle-down"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ banner-->
+  <!--service-->
+  <section id="service" class="section-padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 col-sm-4">
+          <h2 class="ser-title">Our Services</h2>
+          <hr class="botm-line">
+          <p class="jstfy">
+            We offer subsidized health loans to low income people in the informal and formal sector to cover medical bills, services and supplies at our subscribed medical facilities. The loans are used for example by expectant mothers to buy mama kits, delivery mats, cotton and other supplies. For mothers, fathers, next of kin and care takers they use the loans to purchase medicines like malaria drugs for their families such as children.  
+          </p>
+        </div>
+        <div class="col-md-4 col-sm-4">
+          <div class="service-info">
+            <!-- <div class="icon">
+              <i class="fa fa-stethoscope"></i>
+            </div> -->
+            <div class="icon-info">
+              <h4>Basic Category</h4>(10% interest on Payment)<br><br>
+              <table style="width: 100%;">
+                <tr><td>Level One</td><td>- UGX 5,000</td></tr>
+                <tr><td>Level Two</td><td>- UGX 20,000</td></tr>
+                <tr><td>Level Three</td><td>- UGX 35,000</td></tr>
+                <tr><td>Level Four</td><td>- UGX 50,000</td></tr>
+              </table>
+            
+            </div>
+          </div>
+          <div class="service-info">
+            <div class="icon-info">
+              <h4>Regular Category</h4>(9% interest on Payment)<br><br>
+              <table style="width: 100%;">
+                <tr><td>Level One</td><td>- UGX 65,000</td></tr>
+                <tr><td>Level Two</td><td>- UGX 80,000</td></tr>
+                <tr><td>Level Three</td><td>- UGX 95,000</td></tr>
+                <tr><td>Level Four</td><td>- UGX 110,000</td></tr>
+              </table>
+            
+            </div>
+          </div>
+        </div>
+        <div class="col-md-4 col-sm-4">
+          <div class="service-info">
+            
+            <div class="icon-info">
+              <h4>Business Category</h4>(8% interest on Payment)<br><br>
+              <table style="width: 100%;">
+                <tr><td>Level One</td><td>- UGX 150,000</td></tr>
+                <tr><td>Level Two</td><td>- UGX 175,000</td></tr>
+                <tr><td>Level Three</td><td>- UGX 200,000</td></tr>
+                <tr><td>Level Four</td><td>- UGX 225,000</td></tr>
+              </table>
+            
+            </div>
+          </div>
+          <div class="service-info">
+            
+            <div class="icon-info">
+              <h4>Advanced Category</h4>(7% interest on Payment)<br><br>
+              <table style="width: 100%;">
+                <tr><td>Level One</td><td>- UGX 250,000</td></tr>
+                <tr><td>Level Two</td><td>- UGX 350,000</td></tr>
+                <tr><td>Level Three</td><td>- UGX 450,000</td></tr>
+                <tr><td>Level Four</td><td>- UGX 550,000</td></tr>
+              </table>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <section id="about" class="section-padding">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-lg-11">
+
+        <h3>About Us</h3>
+
+        <p class="jstfy">
+          
+            Topowa is a subsidiary of Medical Street international Co. Ltd a registered company with Uganda registration service bureau operating within the stipulated laws of the country and operates as Medical Street online. It’s a health loan and saving startup platform for low income people looking forward to accessing quality healthcare services and supplies at medical facilities such as hospitals, clinics, pharmacies, laboratory’s and diagnostic centers. This is to ensure good health and wellbeing of individuals in order to attain the sustainable development goals. 
+
+        </p>
+
+        <p class="jstfy">
+          
+            Founded in 2018, Topowa provides access to quality healthcare to needs of the poor by offering subsidized loans to qualifying men and women of Uganda working both in the informal and formal sectors. We provide easy, convenient and affordable small loans to struggling patients and families to ensure they have access to medical facilities and supplies. The small loans are paid directly to medical facilities for treatment and diagnosis as well as buying patients supplies such as medicines and sundries from pharmacies. The families and patients are expected to pay back their small loans in installments or wholesome amount in a given amount of time at a specific interest rate of less than 10%. Topowa addresses critical needs in key areas of health including but not limited to neonatal, child and maternal health, malaria, water borne diseases, and other non-communicable diseases to ensure universal health coverage.  
+
+            A strong customer focused approach, conformance to standards and constant quest for top class quality services have enabled the startup to sustain local clients in the major areas of Uganda. Topowa concentrates on individuals working in the informal sector including but not limited to Boda riders, Taxi drivers, market vendors, local shop attendants, Saccos, restaurants and small business owners as well as workers.  We provide health coverage to all patients from neonates to adults who qualify for our services that are indirectly or directly connected with our customers.
+
+        </p>
+
+        <h3>Our Vision Statement</h3>
+        <p class="jstfy">
+        To ensure healthy lives and promote wellbeing for all ages of people at low income status.
+        </p>
+        <h3>Our Mission Statement</h3>
+        <p class="jstfy">
+          To provide access to quality healthcare services to health needs of low income individuals that is easy, convenient and affordable for all. 
+        </p>
+
+        <h3>Our Goal</h3>
+        <p class="jstfy">To support the low income earners have access to quality health care at affordable out of pocket planned payments.   </p>
+
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <!--/ about-->
+  <!--doctor team-->
+<!--   <section id="doctor-team" class="section-padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="ser-title">Meet Our Doctors!</h2>
+          <hr class="botm-line">
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-6">
+          <div class="thumbnail">
+            <img src="img/doctor1.jpg" alt="..." class="team-img">
+            <div class="caption">
+              <h3>Jessica Wally</h3>
+              <p>Doctor</p>
+              <ul class="list-inline">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-6">
+          <div class="thumbnail">
+            <img src="img/doctor2.jpg" alt="..." class="team-img">
+            <div class="caption">
+              <h3>Iai Donas</h3>
+              <p>Doctor</p>
+              <ul class="list-inline">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-6">
+          <div class="thumbnail">
+            <img src="img/doctor3.jpg" alt="..." class="team-img">
+            <div class="caption">
+              <h3>Amanda Denyl</h3>
+              <p>Doctor</p>
+              <ul class="list-inline">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3 col-sm-3 col-xs-6">
+          <div class="thumbnail">
+            <img src="img/doctor4.jpg" alt="..." class="team-img">
+            <div class="caption">
+              <h3>Jason Davis</h3>
+              <p>Doctor</p>
+              <ul class="list-inline">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> -->
+  <!--/ doctor team-->
+
+  <!--cta 2-->
+  <section id="cta-2" class="section-padding">
+    <div class="container">
+      <div class=" row">
+        <div class="col-md-2"></div>
+        
+        <div class="col-md-2"></div>
+      </div>
+    </div>
+  </section>
+  <!--cta-->
+  <!--contact-->
+  <section id="contact" class="section-padding">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h2 class="ser-title">Contact us</h2>
+          <hr class="botm-line">
+        </div>
+        <div class="col-md-4 col-sm-4">
+          <h3>Contact Info</h3>
+          <div class="space"></div>
+          <p><i class="fa fa-map-marker fa-fw pull-left fa-2x"></i>Rwenzori Towers<br> Nakasero, Kampala</p>
+          <div class="space"></div>
+          <p><i class="fa fa-envelope-o fa-fw pull-left fa-2x"></i>info@toppowa.com</p>
+          <div class="space"></div>
+          <p><i class="fa fa-phone fa-fw pull-left fa-2x"></i>+256(0)705363512</p>
+        </div>
+        <div class="col-md-8 col-sm-8 marb20">
+          <div class="contact-info">
+            <h3 class="cnt-ttl">Having Any Query! Or Book an appointment</h3>
+            <div class="space"></div>
+            <div id="sendmessage">Your message has been sent. Thank you!</div>
+            <div id="errormessage"></div>
+            <form action="" method="post" role="form" class="contactForm">
+              <div class="form-group">
+                <input type="text" name="name" class="form-control br-radius-zero" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <input type="email" class="form-control br-radius-zero" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <input type="text" class="form-control br-radius-zero" name="subject" id="subject" placeholder="Subject" data-rule="minlen:4" data-msg="Please enter at least 8 chars of subject" />
+                <div class="validation"></div>
+              </div>
+              <div class="form-group">
+                <textarea class="form-control br-radius-zero" name="message" rows="5" data-rule="required" data-msg="Please write something for us" placeholder="Message"></textarea>
+                <div class="validation"></div>
+              </div>
+
+              <div class="form-action">
+                <button type="submit" class="btn btn-form">Send Message</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!--/ contact-->
+  <!--footer-->
+  <footer id="footer">
+    <div class="top-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4 col-sm-4 marb20">
+            <div class="ftr-tle">
+              <h4 class="white no-padding">About Us</h4>
+            </div>
+            <div class="info-sec">
+              <p>Praesent convallis tortor et enim laoreet, vel consectetur purus latoque penatibus et dis parturient.</p>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 marb20">
+            <div class="ftr-tle">
+              <h4 class="white no-padding">Quick Links</h4>
+            </div>
+            <div class="info-sec">
+              <ul class="quick-info">
+                <li><a href="#banner"><i class="fa fa-circle"></i>Home</a></li>
+                <li><a href="#service"><i class="fa fa-circle"></i>Service</a></li>
+                <!-- <li><a href="#contact"><i class="fa fa-circle"></i>Appointment</a></li> -->
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-4 marb20">
+            <div class="ftr-tle">
+              <h4 class="white no-padding">Follow us</h4>
+            </div>
+            <div class="info-sec">
+              <ul class="social-icon">
+                <li class="bglight-blue"><a href="https://www.facebook.com/Toppowa"><i class="fa fa-facebook"></a></i></li>
+                <!-- <li class="bgred"><i class="fa fa-google-plus"></i></li> -->
+                <li class="bgdark-blue"><i class="fa fa-linkedin"></i></li>
+                <li class="bglight-blue"><i class="fa fa-twitter"></i></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-line">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center">
+            © 2018 Toppowa. All Rights Reserved
+            <div class="credits">
+              <!--
+                All the links in the footer should remain intact.
+                You can delete the links only if you purchased the pro version.
+                Licensing information: https://bootstrapmade.com/license/
+                Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Medilab
+              -->
+         
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!--/ footer-->
+
+  <script>
+  //Array of images which you want to show: Use path you want.
+  var images=new Array('img/a.jpg','img/b.jpg','img/c.jpg');
+  var nextimage=0;
+  doSlideshow();
+
+  function doSlideshow(){
+      if(nextimage>=images.length){nextimage=0;}
+      $('.global-header')
+      .css('background-image','url("'+images[nextimage++]+'")')
+      .fadeIn(500,function(){
+          setTimeout(doSlideshow,1000);
+      });
+  }
+
+  	document.getElementById("demo").innerHTML = doSlideshow();
+
+
+
+  </script>
+
+
+  <script src="{{URL::asset('Assets/js/jquery.min.js')}}"></script>
+  <script src="{{URL::asset('Assets/js/jquery.easing.min.js')}}"></script>
+  <script src="{{URL::asset('Assets/js/bootstrap.min.js')}}"></script>
+  <script src="{{URL::asset('Assets/js/custom.js')}}"></script>
+  <script src="{{URL::asset('Assets/contactform/contactform.js')}}"></script>
+
+</body>
+
+</html>
